30741c7bb3332b1895fd0e99158e843b66c777ce
Eric Burel <eric.burel.0@gmail.com>
2017-03-31 10:47:16 +0200

    More general regexp
    This allow for more flexible patterns :
    - using a plural (stories instead of story), since the file can contain many stories
    - directly name the file `story.js` or `stories.js`, so you can have `components/MyComponents/stories.js`.

--- .storybook/config.js
+++ .storybook/config.js
@@ -6,7 +6,7 @@ import pt from 'react-intl/locale-data/pt';
 setAddon(IntlAddon);
 addLocaleData(pt);
 
-const req = require.context('../app', true, /.story.js$/);
+const req = require.context('../app', true, /(stor(y|ies).js|.stor(y|ies).js$)/);
 
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
